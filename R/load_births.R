d3a134aa053c74785695773759b5459341630f6e
Diego Lara <diego.lara.de.andres@ons.gov.uk>
2024-08-20 21:10:54 +0200

    Feat: Malawi cleaning

--- R/load_births.R
+++ R/load_births.R
@@ -2,29 +2,27 @@ print("Importing and processing Births")
 
 # Import data
 # This is where you load the births data that you have 
-births_data <- read.csv("./data/raw/birth_data.csv", na.strings = c("\\N", "NA", ""))
+full_births_data <- read.csv("./data/raw/births_malawi_sample.csv", na.strings = c("\\N", "NA", ""))
 
 # Construct a still births indicator
-births_data$birth1j <- ifelse(births_data$number_of_children_born_alive_inclusive == births_data$number_of_children_born_still_alive, 1, 0)
+#births_data$birth1j <- ifelse(births_data$number_of_children_born_alive_inclusive == births_data$number_of_children_born_still_alive, 1, 0)
 
 # Subset the dataframe to keep only the specified columns in order
-my_columns <- c("gender", "PlaceofBirth",	"District_of_Birth",
-                "Type_of_Birth",	"ParentsMarried",	"Mother_Birth_Date",
-                "MotherResidentialCountry", "birthdate", "Date_Registered",
-                "birth1j")  
-births_data <- births_data[, my_columns]
+my_columns <- c("Gender", "Place.of.Birth",	"District.of.Birth",
+                "Type.of.Birth",	"Parents.Married.",	"Mother.Date.of.Birth",
+                "Mother.Physical.Residential.District", "Date.of.Birth", "Date.Registered")  
+births_data <- full_births_data[, my_columns]
 
 # And you have a list of new column names in new_names for specific columns
-new_names <- c("gender" = "Birth2a",
-               "PlaceofBirth" = "Birth1i",
-               "District_of_Birth" = "Birth1c",
-               "Type_of_Birth" = "Birth1g",
-               "ParentsMarried" = "Birth3c",
-               "Mother_Birth_Date" = "Birth3a",
-               "MotherResidentialCountry" = "Birth3l",
-               "birthdate" = "Birth1a",
-               "Date_Registered" = "Birth1b",
-               "birth1j" = "Birth1j",)
+new_names <- c("birth2a" = "Gender",
+               "birth1i" = "Place.of.Birth",
+               "birth1c" = "District.of.Birth",
+               "birth1g" = "Type.of.Birth",
+               "birth3c" = "Parents.Married.",
+               "birth3a" = "Mother.Date.of.Birth",
+               "birth3l" = "Mother.Physical.Residential.District",
+               "birth1a" = "Date.of.Birth",
+               "birth1b" = "Date.Registered")
 
 # Rename the specified columns to align with the package
 births_data <- rename(births_data, !!!new_names)
